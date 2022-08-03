7ea18b4d621c2d7ffe8e57a28da3e46e5d2a3254
Shaik-Luqmaan <luqmaan.shaik@skyflow.com>
2022-08-03 14:04:36 +0530

    SDK-663/ Fix path in elements
    Signed-off-by: Shaik-Luqmaan <luqmaan.shaik@skyflow.com>

--- src/elements/CVV/index.tsx
+++ src/elements/CVV/index.tsx
@@ -1,41 +1,42 @@
 /*
 	Copyright (c) 2022 Skyflow, Inc. 
 */
-import React, { FC } from 'react';
-import Skyflow from 'skyflow-js';
-import { SkyflowCollectElementProps } from '..';
-import  useCollectListeners  from '../..//hooks/CollectListner';
+import React, { FC } from "react";
+import Skyflow from "skyflow-js";
+import { SkyflowCollectElementProps } from "..";
+import useCollectListeners from "../../hooks/CollectListner";
 
 const CVVElement: FC<SkyflowCollectElementProps> = ({ ...props }) => {
-  const divElement = document.createElement('div');
+  const divElement = document.createElement("div");
   if (props.id) {
-    divElement.setAttribute('id', props.id);
+    divElement.setAttribute("id", props.id);
   } else {
-    divElement.setAttribute('id', 'collectCVVNumber');
+    divElement.setAttribute("id", "collectCVVNumber");
   }
   React.useEffect(() => {
     try {
+      const newElement = props.container.create(
+        {
+          table: props.table,
+          column: props.column,
+          ...props.classes,
+          placeholder: props.placeholder || "",
+          label: props.label || "",
+          type: Skyflow.ElementType.CVV,
+        },
+        { enableCopy: false }
+      );
 
-      const newElement = props.container.create({
-        table: props.table,
-        column: props.column,
-        ...props.classes,
-        placeholder: props.placeholder || '',
-        label: props.label || '',
-        type: Skyflow.ElementType.CVV,
-      }, { enableCopy: false });
+      newElement.mount(props.id ? `#${props.id}` : "#collectCVVNumber");
 
-      newElement.mount(props.id ? `#${props.id}` : '#collectCVVNumber');
-      
-      useCollectListeners(props,newElement)
-      
+      useCollectListeners(props, newElement);
     } catch (e) {
-    // eslint-disable-next-line no-console
+      // eslint-disable-next-line no-console
       console.log(e);
     }
   }, []);
 
-  return <div id={props.id ? props.id : 'collectCVVNumber'}></div>;
+  return <div id={props.id ? props.id : "collectCVVNumber"}></div>;
 };
 
 export default CVVElement;
--- src/elements/CardNumber/index.tsx
+++ src/elements/CardNumber/index.tsx
@@ -1,42 +1,43 @@
 /*
 	Copyright (c) 2022 Skyflow, Inc. 
 */
-import React, { FC } from 'react';
-import Skyflow from 'skyflow-js';
-import { SkyflowCollectElementProps } from '..';
-import  useCollectListeners  from '../..//hooks/CollectListner';
-
+import React, { FC } from "react";
+import Skyflow from "skyflow-js";
+import { SkyflowCollectElementProps } from "..";
+import useCollectListeners from "../../hooks/CollectListner";
 
 const CardNumberElement: FC<SkyflowCollectElementProps> = ({ ...props }) => {
-  const divElement = document.createElement('div');
+  const divElement = document.createElement("div");
   if (props.id) {
-    divElement.setAttribute('id', props.id);
+    divElement.setAttribute("id", props.id);
   } else {
-    divElement.setAttribute('id', 'collectCardNumber');
+    divElement.setAttribute("id", "collectCardNumber");
   }
 
   React.useEffect(() => {
     try {
-      const newElement = props.container.create({
-        table: props.table,
-        column: props.column,
-        ...props.classes,
-        placeholder: props.placeholder || '',
-        label: props.label || '',
-        type: Skyflow.ElementType.CARD_NUMBER,
-      }, { enableCopy: true });
+      const newElement = props.container.create(
+        {
+          table: props.table,
+          column: props.column,
+          ...props.classes,
+          placeholder: props.placeholder || "",
+          label: props.label || "",
+          type: Skyflow.ElementType.CARD_NUMBER,
+        },
+        { enableCopy: true }
+      );
 
-      newElement.mount(props.id ? `#${props.id}` : '#collectCardNumber');
+      newElement.mount(props.id ? `#${props.id}` : "#collectCardNumber");
 
-      useCollectListeners(props,newElement)
-
+      useCollectListeners(props, newElement);
     } catch (e) {
       // eslint-disable-next-line no-console
       console.log(e);
     }
   }, []);
 
-  return <div id={props.id ? props.id : 'collectCardNumber'}></div>;
+  return <div id={props.id ? props.id : "collectCardNumber"}></div>;
 };
 
 export default CardNumberElement;
