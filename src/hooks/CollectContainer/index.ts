996eac91d7fc37e9a2a79eb82746adf0efd1bab2
Shaik-Luqmaan <luqmaan.shaik@skyflow.com>
2022-09-22 11:12:13 +0530

    SDK-749/ Fix imports in hooks
    Signed-off-by: Shaik-Luqmaan <luqmaan.shaik@skyflow.com>

--- src/hooks/CollectContainer/index.ts
+++ src/hooks/CollectContainer/index.ts
@@ -2,7 +2,8 @@
 	Copyright (c) 2022 Skyflow, Inc. 
 */
 import Skyflow from 'skyflow-js'
-import CollectContainer from 'skyflow-js/types/core/external/collect/CollectContainer'
+import CollectContainer from 'skyflow-js/types/core/external/collect/collect-container'
+
 import { useSkyflow } from '../../core/hook'
 
 const useCollectContainer = (): CollectContainer => {
