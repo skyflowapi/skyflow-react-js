3a298f33fe3a07c108c71905c9966fe30c930eba
yaswanth-pula-skyflow <yaswanthsai.pula@skyflow.com>
2023-01-04 18:49:52 +0530

    SK-288 add useComposableContainer hook.

---
+++ src/elements/ComposableContainer/index.tsx
@@ -0,0 +1,40 @@
+/*
+  Copyright (c) 2022 Skyflow, Inc. 
+*/
+import React, { FC, useState } from 'react'
+import { ELEMENT_CREATED } from '../../utils/constants'
+import EventEmitter from '../../utils/event-emitter'
+
+const ComposableContainer: FC<any> = ({ children, ...props }) => {
+
+  const [currentCount, setCurrentCount] = useState(0);
+
+  const eventEmitter: EventEmitter = new EventEmitter();
+
+  eventEmitter.on(ELEMENT_CREATED, (data) => {
+    setCurrentCount((prev) => prev + 1);
+  });
+
+  const iterateOverChildren = (children) => {
+    return React.Children.map(children, (child) => {
+      if (!React.isValidElement(child)) return child;
+      return React.cloneElement(child, { eventEmitter: eventEmitter } as Partial<unknown>)
+    })
+  };
+
+  React.useEffect(() => {
+    try {
+      if (currentCount === React.Children.count(children))
+        props.container.mount(props.id ? `#${props.id}` : '#composableContainer');
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e)
+    }
+  }, [currentCount])
+
+  return <div id={props.id ? props.id : 'composeContainer'}>
+    {iterateOverChildren(children)}
+  </div>
+}
+
+export default ComposableContainer;
---
+++ src/utils/event-emitter/index.ts
@@ -0,0 +1,74 @@
+class EventEmitter {
+  events: Record<string, { priority: boolean; callback: any }[]>
+
+  constructor() {
+    this.events = {}
+  }
+
+  on(event: string, callback: any, priority = false): void {
+    if (this.events[event]) {
+      this.events[event].push({ priority, callback })
+    } else {
+      this.events[event] = [{ priority, callback }]
+    }
+  }
+
+  off(event: string, callback: any): void {
+    const eventCallbacks = this.events[event]
+
+    if (!eventCallbacks) {
+      return
+    }
+
+    const indexOfCallback = eventCallbacks.findIndex((iEvent) => iEvent.callback === callback)
+
+    eventCallbacks.splice(indexOfCallback, 1)
+  }
+
+  // eslint-disable-next-line no-underscore-dangle
+  _emit(event: string, ...args): void {
+    const eventCallbacks = this.events[event]
+
+    if (!eventCallbacks) {
+      return
+    }
+
+    eventCallbacks.forEach((iEvent) => {
+      iEvent.callback(...args)
+    })
+  }
+
+  hasListener(event: string): boolean {
+    const eventCallbacks = this.events[event]
+
+    if (!eventCallbacks) {
+      return false
+    }
+
+    return eventCallbacks.length > 0
+  }
+
+  resetEvents() {
+    const tempEvents: any = {}
+    Object.keys(this.events).forEach((eventName) => {
+      const events = this.events[eventName]
+      if (events && events.length > 0) {
+        const newEvents: any[] = []
+        events.forEach((event) => {
+          if (event.priority) newEvents.push(event)
+        })
+        tempEvents[eventName] = newEvents
+      }
+    })
+
+    this.events = tempEvents
+  }
+
+  static createChild(ChildObject): void {
+    ChildObject.prototype = Object.create(EventEmitter.prototype, {
+      constructor: ChildObject,
+    })
+  }
+}
+
+export default EventEmitter
