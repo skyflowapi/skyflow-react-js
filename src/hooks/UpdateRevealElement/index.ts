65396a1da8cbe93cd16e801f4a0241ef4a2c0c25
skyflow-vivek <vivek.jain@skyflow.com>
2023-12-13 21:43:15 +0530

    SK-1286 Ability to update element props dynamically
    - SK-1287 Update Collect Element props dynamically
    - SK-1288 Update Reveal Element props dynamically
    - SK-1288 Update File Render Element props dynamically

---
+++ src/hooks/UpdateRevealElement/index.ts
@@ -0,0 +1,60 @@
+import React from 'react'
+import { SkyflowRevealElementProps } from 'elements'
+import RevealElement from 'skyflow-js/types/core/external/reveal/reveal-element'
+
+const useUpdateRevealElement = (
+  props: SkyflowRevealElementProps,
+  element: RevealElement | null,
+): void => {
+  React.useEffect(() => {
+    if (element) {
+      if (props?.container !== undefined) {
+        element.update({
+          altText: props.altText,
+        })
+      }
+    }
+  }, [props.altText])
+
+  React.useEffect(() => {
+    if (element) {
+      if (props?.container !== undefined) {
+        element.update({
+          classes: props.classes,
+        })
+      }
+    }
+  }, [props.classes])
+
+  React.useEffect(() => {
+    if (element) {
+      if (props?.container !== undefined) {
+        element.update({
+          token: props.token,
+        })
+      }
+    }
+  }, [props.token])
+
+  React.useEffect(() => {
+    if (element) {
+      if (props?.container !== undefined) {
+        element.update({
+          label: props.label,
+        })
+      }
+    }
+  }, [props.label])
+
+  React.useEffect(() => {
+    if (element) {
+      if (props?.container !== undefined) {
+        element.update({
+          redaction: props.redaction,
+        })
+      }
+    }
+  }, [props.redaction])
+}
+
+export default useUpdateRevealElement
