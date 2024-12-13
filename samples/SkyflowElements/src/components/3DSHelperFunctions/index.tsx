a85ad5527285522f7a7b72c029fc60d85c2db6ed
Shashank prajapati <shashank-prajapathi@skyflow.com>
2024-12-13 14:59:27 +0530

    FINT-644:3DS showChallenge helper function

--- samples/SkyflowElements/src/components/3DSHelperFunctions/index.tsx
+++ samples/SkyflowElements/src/components/3DSHelperFunctions/index.tsx
@@ -14,9 +14,34 @@ const ThreeDSHelperFunctions = () => {
       browserDetailsDiv.innerHTML = JSON.stringify(browserDetails)
     }
   }
+
+  const showChallenge = () => {
+    const actionButtons = document.getElementById('actionButtons')
+    const challengeWindow = document.getElementById('challengeWindow')
+    if (challengeWindow) {
+      const challengeIFrame = threeDS.showChallenege('<acs-url>', '<c-req>', '04', challengeWindow)
+      challengeIFrame.addEventListener('load', function () {
+        if ( actionButtons && actionButtons.hidden) {
+          challengeIFrame.hidden = true
+          actionButtons.hidden = false
+        } else if(actionButtons) {
+          actionButtons.hidden = true
+        }
+      })
+    }
+  }
   return (
     <div className='CollectElements' style={{ width: '300px' }}>
-      <button onClick={generateBrowserDetails}>Generate Browser Details</button>
+      <div id='actionButtons'>
+        <h3>3DS Browser Details</h3>
+        <div>
+          <button onClick={generateBrowserDetails}>Generate Browser Details</button>
+          <button onClick={showChallenge}>Show Challenge</button>
+        </div>
+        <div></div>
+        <pre id='threeDSBrowserDetails'></pre>
+      </div>
+      <div id='challengeWindow'></div>
       <div id='threeDSBrowserDetails'></div>
     </div>
   )
